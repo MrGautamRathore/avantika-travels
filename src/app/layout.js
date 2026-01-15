1053cdc0bd65c9ab6730b3d60196e31ce27f2c6d
AaminSPatel <locomail112@gmail.com>
2026-01-15 23:45:30 +0530

    inhanced seo

--- src/app/layout.js
+++ src/app/layout.js
@@ -72,7 +72,7 @@ export default function RootLayout({ children }) {
 <script async src="https://www.googletagmanager.com/gtag/js?id=G-WC73H9S95D"></script>
 <script>
   window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
+  function gtag(){dataLayer.push(arguments)}
   gtag('js', new Date());
 
   gtag('config', 'G-WC73H9S95D');
